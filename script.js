e3cbb0561c23ec6a07fe23053a5f8800eab30521
hironmoy-ray <hpray35@gmail.com>
2024-11-26 17:04:00 +0600

    Project Completed

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-for (let country in countryList) {
-  console.log(country, countryList[country]);
-}
+const url =
+  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json";
 
 const dropdown = document.querySelectorAll(".dropdown select");
 
@@ -21,6 +20,18 @@ for (let select of dropdown) {
   });
 }
 
+const fromField = document.querySelector(".from select");
+let fromFieldCur = fromField.value.toLowerCase();
+fromField.addEventListener("change", () => {
+  fromFieldCur = fromField.value.toLowerCase();
+});
+
+const toField = document.querySelector(".to select");
+let toFieldCur = toField.value.toLowerCase();
+toField.addEventListener("change", () => {
+  toFieldCur = toField.value.toLowerCase();
+});
+
 function updateFlag(element) {
   let curCode = element.value;
   let country = countryList[curCode];
@@ -28,3 +39,29 @@ function updateFlag(element) {
   let img = element.parentElement.querySelector("img");
   img.src = newSrc;
 }
+
+const btn = document.querySelector("form button");
+
+btn.addEventListener("click", async (e) => {
+  e.preventDefault();
+  let inputField = document.querySelector(".amount input");
+  let inputVal = inputField.value;
+  if (inputVal == "" || inputVal < 1) {
+    inputVal = 1;
+    inputField.value = "1";
+  }
+  //   console.log(fromFieldCur);
+  //   console.log(toFieldCur);
+
+  //   console.log(inputVal);
+  let res = await fetch(url);
+  let data = await res.json();
+  let fromVal = data.eur[fromFieldCur];
+  let toVal = data.eur[toFieldCur];
+  //   console.log(fromVal, toVal);
+  let rate = (toVal / fromVal) * inputVal;
+  //   console.log(rate);
+
+  const msgField = document.querySelector(".msg");
+  msgField.innerText = `${inputVal} ${fromFieldCur.toUpperCase()} = ${rate} ${toFieldCur.toUpperCase()}`;
+});
